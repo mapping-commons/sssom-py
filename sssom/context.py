f46967a7a1d0c3371ee9dffc82e62e6b393e6e5f
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-02-22 10:12:03 +0000

    Making sure context can be read in distro

--- sssom/context.py
+++ sssom/context.py
@@ -1,11 +1,15 @@
 import os
 import json
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
 
-cwd = os.path.abspath(os.path.dirname(__file__))
-DEFAULT_CONTEXT_PATH = f'{cwd}/../schema/sssom.context.jsonld'
-
+#cwd = os.path.abspath(os.path.dirname(__file__))
+DEFAULT_CONTEXT_PATH = 'sssom.context.jsonld'
 
 def get_jsonld_context():
-    with open(DEFAULT_CONTEXT_PATH) as json_file:
+    with pkg_resources.open_text('sssom',DEFAULT_CONTEXT_PATH) as json_file:
         contxt = json.load(json_file)
     return contxt
