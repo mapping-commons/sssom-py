2b8f8b35d988019d2f914240e75217d791ab2d7c
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-02-22 10:22:27 +0000

    Update context.py

--- sssom/context.py
+++ sssom/context.py
@@ -6,10 +6,11 @@ except ImportError:
     # Try backported to PY<37 `importlib_resources`.
     import importlib_resources as pkg_resources
 
-#cwd = os.path.abspath(os.path.dirname(__file__))
+
+cwd = os.path.abspath(os.path.dirname(__file__))
 DEFAULT_CONTEXT_PATH = 'sssom.context.jsonld'
 
 def get_jsonld_context():
-    with pkg_resources.open_text('sssom',DEFAULT_CONTEXT_PATH) as json_file:
+    with open(os.path.join(cwd, DEFAULT_CONTEXT_PATH)) as json_file:
         contxt = json.load(json_file)
     return contxt
