6bf19cb7ddfca296b0abce7e7b27a2e0f89753ca
Harshad <hrshdhgd@users.noreply.github.com>
2021-06-08 11:17:38 -0500

    check if path is valid else throws Exception #55

--- sssom/io.py
+++ sssom/io.py
@@ -5,7 +5,7 @@ from sssom.sssom_datamodel import MappingSet
 from .parsers import get_parsing_function, from_tsv, split_dataframe
 from .writers import get_writer_function, write_tsv, write_tsvs
 from .context import get_default_metadata
-import json
+import validators
 import yaml
 import logging
 
@@ -30,7 +30,7 @@ def write_sssom(mset: MappingSetDataFrame, output: str = None) -> None:
             for line in lines:
                 stream.write(line)
 
-def convert_file(input: str, output: str = None, input_format: str = None, output_format: str = None, context_path=None,
+def convert_file(input_path: str, output: str = None, input_format: str = None, output_format: str = None, context_path=None,
                  read_func=None, write_func=None):
     """
     converts from one format to another
@@ -43,15 +43,20 @@ def convert_file(input: str, output: str = None, input_format: str = None, outpu
     :param write_func
     :return:
     """
-    curie_map, meta = get_metadata_and_curie_map(metadata_path=context_path, curie_map_mode="metadata_only")
+    if validators.url(input_path) or os.path.exists(input_path):
+        curie_map, meta = get_metadata_and_curie_map(metadata_path=context_path, curie_map_mode="metadata_only")
 
-    if read_func is None:
-        read_func = get_parsing_function(input_format, input)
-    doc = read_func(input,curie_map=curie_map)
+        if read_func is None:
+            read_func = get_parsing_function(input_format, input_path)
+        doc = read_func(input_path,curie_map=curie_map)
 
-    if write_func is None:
-        write_func, fileformat = get_writer_function(output_format, output)
-    write_func(doc, output, fileformat=fileformat, context_path=context_path)
+        if write_func is None:
+            write_func, fileformat = get_writer_function(output_format, output)
+        write_func(doc, output, fileformat=fileformat, context_path=context_path)
+        
+    else:
+        raise Exception(f"{input_path} is not a valid file path or url.")
+    
 
 
 def parse_file(input_path: str, output_path: str = None, input_format: str = None, metadata_path: str = None, curie_map_mode: str = None):
@@ -64,10 +69,14 @@ def parse_file(input_path: str, output_path: str = None, input_format: str = Non
     :param curie_map_mode:
     :return:
     """
-    curie_map, meta = get_metadata_and_curie_map(metadata_path=metadata_path, curie_map_mode=curie_map_mode)
-    parse_func = get_parsing_function(input_format, input_path)
-    doc = parse_func(input_path, curie_map=curie_map, meta=meta)
-    write_tsv(doc, output_path)
+
+    if validators.url(input_path) or os.path.exists(input_path):
+        curie_map, meta = get_metadata_and_curie_map(metadata_path=metadata_path, curie_map_mode=curie_map_mode)
+        parse_func = get_parsing_function(input_format, input_path)
+        doc = parse_func(input_path, curie_map=curie_map, meta=meta)
+        write_tsv(doc, output_path)
+    else:
+        raise Exception(f"{input_path} is not a valid file path or url.")
 
 def validate_file(input_path: str):
     """
@@ -91,9 +100,12 @@ def split_file(input_path: str, output_directory: str):
     :param output_directory: the directory in which to export the split files
     :return:
     """
-    msdf = from_tsv(input_path)
-    splitted = split_dataframe(msdf)
-    write_tsvs(splitted, output_directory)
+    if validators.url(input_path) or os.path.exists(input_path):
+        msdf = from_tsv(input_path)
+        splitted = split_dataframe(msdf)
+        write_tsvs(splitted, output_directory)
+    else:
+        raise Exception(f"{input_path} is not a valid file path or url.")
 
 
 def get_metadata_and_curie_map(metadata_path, curie_map_mode: str = "metadata_only"):
