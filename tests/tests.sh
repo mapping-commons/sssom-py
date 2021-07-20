622876afc57b3d140a0a8bdcd8cbb79eb9fc6728
Harshad <hrshdhgd@users.noreply.github.com>
2021-07-20 11:25:03 -0500

    test update fro #55 and #100

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,22 +3,49 @@ set -e
 INPUT_FILE_1='tests/data/basic.tsv'
 INPUT_FILE_2='tests/data/basic2.tsv'
 INPUT_FILE_3='tests/data/basic3.tsv'
+INPUT_URL_1='https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic.tsv'
+INPUT_URL_2='https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic2.tsv'
+INPUT_URL_3='https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic3.tsv'
 OUTPUT_DIR='tests/tmp/'
 QUERY_1="SELECT * FROM df1 WHERE confidence>0.5 ORDER BY confidence"
 
-sssom parse --input $INPUT_FILE_1 --output $OUTPUT_DIR/parsed_basic.tsv --input-format tsv --curie-map-mode merged
-sssom split --input $INPUT_FILE_1 --output-directory $OUTPUT_DIR
+sssom parse $INPUT_FILE_1 --output $OUTPUT_DIR/parsed_basic.tsv --input-format tsv --curie-map-mode merged
+sssom parse $INPUT_URL_1 --output $OUTPUT_DIR/parsed_basic.tsv --input-format tsv --curie-map-mode merged
 
-sssom convert --input $INPUT_FILE_1 --output $OUTPUT_DIR/converted_basic.tsv --output-format tsv
-sssom validate --input $INPUT_FILE_1
-sssom ptable --input $INPUT_FILE_1
-sssom dedupe --input $INPUT_FILE_1 --output $OUTPUT_DIR/deduped_basic.tsv
+sssom split $INPUT_FILE_1 --output-directory $OUTPUT_DIR
+sssom split $INPUT_URL_1 --output-directory $OUTPUT_DIR
+
+sssom convert $INPUT_FILE_1 --output $OUTPUT_DIR/converted_basic.tsv --output-format tsv
+sssom convert $INPUT_URL_1 --output $OUTPUT_DIR/converted_basic.tsv --output-format tsv
+
+sssom validate $INPUT_FILE_1
+sssom validate $INPUT_URL_1
+
+sssom ptable $INPUT_FILE_1
+sssom ptable $INPUT_URL_1
+
+sssom dedupe $INPUT_FILE_1 --output $OUTPUT_DIR/deduped_basic.tsv
+sssom dedupe $INPUT_URL_1 --output $OUTPUT_DIR/deduped_basic.tsv
+
 sssom dosql -q "$QUERY_1" $INPUT_FILE_1 -o $OUTPUT_DIR/dosql_output.tsv
-#sssom sparql 
-sssom diff $INPUT_FILE_1 $INPUT_FILE_2 -o $OUTPUT_DIR/diff_output.tsv
-sssom partition -d $OUTPUT_DIR $INPUT_FILE_1 $INPUT_FILE_2 
-sssom cliquesummary -i $INPUT_FILE_1 -o $OUTPUT_DIR/cliquesummary_output.tsv
-sssom crosstab -i $INPUT_FILE_1 -o $OUTPUT_DIR/crosstab_output.tsv
-sssom correlations -i $INPUT_FILE_1 -o $OUTPUT_DIR/correlation_output.tsv
-sssom merge $INPUT_FILE_1 $INPUT_FILE_2 $INPUT_FILE_3 -o $OUTPUT_DIR/merged_msdf.tsv
+sssom dosql -q "$QUERY_1" $INPUT_URL_1 -o $OUTPUT_DIR/dosql_output.tsv
 
+#sssom sparql 
+
+sssom diff $INPUT_FILE_1 $INPUT_FILE_2 -o $OUTPUT_DIR/diff_output.tsv
+sssom diff $INPUT_URL_1 $INPUT_URL_2 -o $OUTPUT_DIR/diff_output.tsv
+
+sssom partition -d $OUTPUT_DIR $INPUT_FILE_1 $INPUT_FILE_2 
+sssom partition -d $OUTPUT_DIR $INPUT_URL_1 $INPUT_URL_2
+
+sssom cliquesummary $INPUT_FILE_1 -o $OUTPUT_DIR/cliquesummary_output.tsv
+sssom cliquesummary $INPUT_URL_1 -o $OUTPUT_DIR/cliquesummary_output.tsv
+
+sssom crosstab $INPUT_FILE_1 -o $OUTPUT_DIR/crosstab_output.tsv
+sssom crosstab $INPUT_URL_1 -o $OUTPUT_DIR/crosstab_output.tsv
+
+sssom correlations $INPUT_FILE_1 -o $OUTPUT_DIR/correlation_output.tsv
+sssom correlations $INPUT_URL_1 -o $OUTPUT_DIR/correlation_output.tsv
+
+sssom merge $INPUT_FILE_1 $INPUT_FILE_2 $INPUT_FILE_3 -o $OUTPUT_DIR/merged_msdf.tsv
+sssom merge $INPUT_URL_1 $INPUT_URL_2 $INPUT_URL_3 -o $OUTPUT_DIR/merged_msdf.tsv
