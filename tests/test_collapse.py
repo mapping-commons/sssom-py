0ece34e8868a55a1f7a75c72af82aa96c80ffb2e
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-08-30 17:40:49 +0300

    Update test_collapse.py

--- tests/test_collapse.py
+++ tests/test_collapse.py
@@ -22,16 +22,14 @@ class TestCollapse(unittest.TestCase):
 
     def test_df(self):
         df = self.df
-        print(df[0:20])
-        self.assertTrue(True)
+        self.assertEquals(len(df), 141, f"Dataframe should have a different number of rows {df.head(10)}")
 
     def test_collapse(self):
         df = collapse(self.df)
-        print(df[0:20])
+        self.assertEquals(len(df), 92, f"Dataframe should have a different {df.head(10)}")
 
     def test_filter(self):
         df = filter_redundant_rows(self.df)
-        print(df[0:20])
         self.assertEqual(len(df.index), 141)
 
     def test_ptable(self):
@@ -42,7 +40,7 @@ class TestCollapse(unittest.TestCase):
 
     def test_groupings(self):
         mappings = group_mappings(self.df)
-        self.assertEqual(len(mappings), 3)
+        self.assertEqual(len(mappings), 91)
 
     def test_diff(self):
         diff = compare_dataframes(self.df, self.df)
@@ -50,25 +48,25 @@ class TestCollapse(unittest.TestCase):
         assert len(diff.unique_tuples2) == 0
         assert len(diff.common_tuples) == 91
         diff_df = diff.combined_dataframe
-        print(len(diff_df.index))
-        print(diff_df[0:20])
+        # print(len(diff_df.index))
+        # print(diff_df[0:20])
         assert len(diff_df.index) > 100
         for c in diff_df["comment"]:
             assert c.startswith("COMMON_TO_BOTH")
         output = sqldf("select * from diff_df where comment != ''")
-        print(output)
-        print(diff)
+        # print(output)
+        # print(diff)
 
         df2 = parse(f"{data_dir}/basic2.tsv")
         diff = compare_dataframes(self.df, df2)
-        print(len(diff.unique_tuples1))
-        print(len(diff.unique_tuples2))
-        print(len(diff.common_tuples))
+        # print(len(diff.unique_tuples1))
+        # print(len(diff.unique_tuples2))
+        # print(len(diff.common_tuples))
         assert len(diff.unique_tuples1) == 15
         assert len(diff.unique_tuples2) == 3
         assert len(diff.common_tuples) == 76
         # totlen = len(diff.unique_tuples1) + len(diff.unique_tuples2) + len(diff.common_tuples)
         # assert totlen == len(self.df.index) + len(df2.index)
         diff_df = diff.combined_dataframe
-        print(len(diff_df.index))
-        print(diff_df[0:10])
+        # print(len(diff_df.index))
+        # print(diff_df[0:10])
