3abe29b8cc84b46700df8a2804ce16ae9b63583e
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-08-31 19:43:59 +0300

    Fix RDF bugs

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -13,6 +13,7 @@ data_dir = os.path.join(cwd, "data")
 class TestConvert(unittest.TestCase):
     def setUp(self) -> None:
         self.msdf = read_sssom_table(f"{data_dir}/basic.tsv")
+        self.cob = read_sssom_table(f"{data_dir}/cob-to-external.tsv")
 
     def test_df(self):
         df = self.msdf.df
@@ -32,6 +33,18 @@ class TestConvert(unittest.TestCase):
         )
         size = len(results)
         self.assertEqual(size, 90)
+
+    def test_cob_to_owl(self):
+        g = to_owl_graph(self.cob)
+        results = g.query(
+            """SELECT DISTINCT ?e1 ?e2
+                WHERE {
+                  ?e1 <http://www.w3.org/2002/07/owl#equivalentClass> ?e2 .
+                }"""
+        )
+        # g.serialize(destination="tmp/cob-external-test.owl", format="turtle")
+        size = len(results)
+        self.assertEqual(size, 61)
 
     def test_to_rdf(self):
         g = to_rdf_graph(self.msdf)
