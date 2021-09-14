384fb429453ff21efd9fa590cab0f4e58c79a6b1
Harshad <hrshdhgd@users.noreply.github.com>
2021-09-14 10:40:55 -0500

    meta type: Dict[str, str]

--- sssom/context.py
+++ sssom/context.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Any, Mapping, Tuple
+from typing import Any, Dict, Mapping, Tuple
 
 from .external_context import sssom_external_context
 from .internal_context import sssom_context
@@ -49,7 +49,7 @@ def get_default_metadata() -> Tuple[Mapping[str, Any], Mapping[str, Any]]:
     contxt = get_jsonld_context()
     contxt_external = get_external_jsonld_context()
     curie_map = {}
-    meta = {}
+    meta: Dict[str, str] = {}
     for key in contxt["@context"]:
         v = contxt["@context"][key]
         if isinstance(v, str):
