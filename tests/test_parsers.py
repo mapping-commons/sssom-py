ca3514b24a9c3166008c7b7fd8f8708fd56ebffa
Harshad <hrshdhgd@users.noreply.github.com>
2021-12-03 09:35:54 -0600

    reverted back to original url (#178)
    * reverted back to original url
    
    * formatted

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -32,10 +32,7 @@ class TestParse(unittest.TestCase):
         if not os.path.exists(test_out_dir):
             os.mkdir(test_out_dir)
 
-        # self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic.tsv"
-        # ** TEMPORARY EDIT. NEEDS TO ROLL BACK TO PREV LINE***
-        self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/update-sssom/tests/data/basic.tsv"
-
+        self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic.tsv"
         self.rdf_graph_file = f"{test_data_dir}/basic.sssom.rdf"
         self.rdf_graph = Graph()
         self.rdf_graph.parse(self.rdf_graph_file, format="ttl")
