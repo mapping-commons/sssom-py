f4f727e3c54d02925739f75debc3c520a67743f9
Harshad Hegde <hegdehb@gmail.com>
2021-12-07 08:29:33 -0600

    moved sssom.yaml to package dir, new constants.py

--- sssom/__init__.py
+++ sssom/__init__.py
@@ -1,5 +1,4 @@
 """Initializing imports and constants."""
-import os
 
 from .sssom_datamodel import slots  # noqa:401
 from .sssom_datamodel import Mapping, MappingSet  # noqa:401
@@ -11,6 +10,3 @@ from .util import (  # noqa:401
     group_mappings,
     parse,
 )
-
-SCHEMA_PARENT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-SCHEMA_YAML = os.path.join(SCHEMA_PARENT_PATH, "schema/sssom.yaml")
---
+++ sssom/constants.py
@@ -0,0 +1,5 @@
+"""Constants."""
+
+# SCHEMA_PARENT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+# SCHEMA_YAML = os.path.join(SCHEMA_PARENT_PATH, "schema/sssom.yaml")
+SCHEMA_YAML = "sssom/sssom.yaml"
