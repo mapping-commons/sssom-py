bcc0496e5acdb22d0ae965646ae87cf066b96535
Harshad Hegde <hegdehb@gmail.com>
2022-06-13 09:49:29 -0500

    switched link back to master branch

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -32,8 +32,7 @@ class TestParse(unittest.TestCase):
     def setUp(self) -> None:
         """Set up the test case."""
         # TODO: change back to the commented url.
-        # self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic.tsv"
-        self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/sssom-schema-impl/tests/data/basic.tsv"
+        self.df_url = "https://raw.githubusercontent.com/mapping-commons/sssom-py/master/tests/data/basic.tsv"
         self.rdf_graph_file = f"{test_data_dir}/basic.sssom.rdf"
         self.rdf_graph = Graph()
         self.rdf_graph.parse(self.rdf_graph_file, format="ttl")
