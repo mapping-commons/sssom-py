5ff2f51e49bf988779f6c8c6e095476e6e2dcf8e
Harshad Hegde <hegdehb@gmail.com>
2022-08-02 09:49:53 -0500

    added test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,19 @@
 """Test for merging MappingSetDataFrames."""
 import unittest
+from sssom.constants import OBJECT_ID, SUBJECT_ID
 
 from sssom.io import extract_iri
+from sssom.parsers import parse_sssom_table
+from sssom.util import filter_prefixes
 from tests.constants import data_dir
 
 
 class TestIO(unittest.TestCase):
     """A test case for merging msdfs."""
+    def setUp(self) -> None:
+        self.msdf = parse_sssom_table(f"{data_dir}/basic.tsv")
+        self.features = [SUBJECT_ID, OBJECT_ID]
+    
 
     def test_broken_predicate_list(self):
         """Test merging of multiple msdfs."""
@@ -18,3 +25,10 @@ class TestIO(unittest.TestCase):
             if p_iri:
                 iri_list.extend(p_iri)
         self.assertEqual(3, len(iri_list))
+
+    def test_filter_prefixes(self):
+        """Test filtering MSDF by prefixes provided."""
+        prefix_filter_list = ["x","y"]
+        filtered_df = filter_prefixes(self.msdf.df, prefix_filter_list, self.features)
+        self.assertEqual(len(filtered_df), 40)
+
