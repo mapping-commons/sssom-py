8f0c6c82f32c77b6ca2b21329cd44de0aac537c4
Harshad Hegde <hegdehb@gmail.com>
2022-08-09 12:57:57 -0500

    added new test file for reconclie

--- tests/test_reconcile.py
+++ tests/test_reconcile.py
@@ -13,25 +13,25 @@ class TestReconcile(unittest.TestCase):
 
     def setUp(self) -> None:
         """Test up the test case with the third basic example."""
-        self.msdf = parse_sssom_table(data_dir / "basic3.tsv")
+        self.msdf = parse_sssom_table(data_dir / "basic7.tsv")
 
     def test_filter(self):
         """Test filtering returns the right number of rows."""
         df = filter_redundant_rows(self.msdf.df)
-        self.assertEqual(10, len(df.index))
+        self.assertEqual(18, len(df.index))
 
     def test_deal_with_negation(self):
         """Test handling negating returns the right number of rows."""
         df = deal_with_negation(self.msdf.df)
-        self.assertEqual(7, len(df.index))
+        self.assertEqual(5, len(df.index))
 
     def test_merge(self):
         """Test merging two tables."""
         msdf1 = parse_sssom_table(data_dir / "basic.tsv")
-        msdf2 = parse_sssom_table(data_dir / "basic2.tsv")
+        msdf2 = self.msdf
         merged_msdf = merge_msdf(msdf1, msdf2)
 
-        self.assertEqual(122, len(merged_msdf.df))
+        self.assertEqual(107, len(merged_msdf.df))
 
     def test_merge_no_reconcile(self):
         """Test merging two tables without reconciliation."""
