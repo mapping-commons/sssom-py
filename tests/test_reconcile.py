c9565354f7694067b220d96e3475785b4922e922
Harshad Hegde <hegdehb@gmail.com>
2022-08-17 13:01:40 -0500

    retained earlier test and added for basic7.tsv

--- tests/test_reconcile.py
+++ tests/test_reconcile.py
@@ -13,25 +13,32 @@ class TestReconcile(unittest.TestCase):
 
     def setUp(self) -> None:
         """Test up the test case with the third basic example."""
-        self.msdf = parse_sssom_table(data_dir / "basic7.tsv")
+        self.msdf1 = parse_sssom_table(data_dir / "basic3.tsv")
+        self.msdf2 = parse_sssom_table(data_dir / "basic7.tsv")
 
     def test_filter(self):
         """Test filtering returns the right number of rows."""
-        df = filter_redundant_rows(self.msdf.df)
-        self.assertEqual(18, len(df.index))
+        df1 = filter_redundant_rows(self.msdf1.df)
+        self.assertEqual(10, len(df1.index))
+        df2 = filter_redundant_rows(self.msdf2.df)
+        self.assertEqual(18, len(df2.index))
 
     def test_deal_with_negation(self):
         """Test handling negating returns the right number of rows."""
-        df = deal_with_negation(self.msdf.df)
-        self.assertEqual(5, len(df.index))
+        df1 = deal_with_negation(self.msdf1.df)
+        self.assertEqual(7, len(df1.index))
+        df2 = deal_with_negation(self.msdf2.df)
+        self.assertEqual(5, len(df2.index))
 
     def test_merge(self):
         """Test merging two tables."""
-        msdf1 = parse_sssom_table(data_dir / "basic.tsv")
-        msdf2 = self.msdf
-        merged_msdf = merge_msdf(msdf1, msdf2)
+        msdf3 = parse_sssom_table(data_dir / "basic.tsv")
+        merged_msdf1 = merge_msdf(self.msdf1, msdf3)
 
-        self.assertEqual(107, len(merged_msdf.df))
+        self.assertEqual(98, len(merged_msdf1.df))
+
+        merged_msdf2 = merge_msdf(self.msdf2, msdf3)
+        self.assertEqual(107, len(merged_msdf2.df))
 
     def test_merge_no_reconcile(self):
         """Test merging two tables without reconciliation."""
