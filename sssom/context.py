d83d398eb8db03a916ade810c896d63c1d10e473
Harshad <hrshdhgd@users.noreply.github.com>
2022-08-24 12:51:49 -0500

    Fixes #305 and  #306 (#308)
    * Fixes validate  #306
    
    * fix for #305
    
    * blank values for prefixes shouls not be added.
    
    * undo refactor and change logic
    
    * changed metadata check logic
    
    * changed logic to get pref from meta
    
    * formatted
    
    * casting issue
    
    * mypy fix
    
    * mypy fix
    
    * edits after discussion with Nico on PR

--- sssom/context.py
+++ sssom/context.py
@@ -17,7 +17,7 @@ from .typehints import Metadata, MetadataType, PrefixMap
 # EXTERNAL_CONTEXT_PATH = HERE / "sssom.external.context.jsonld"
 
 SSSOM_URI_PREFIX = "https://w3id.org/sssom/"
-SSSOM_BUILT_IN_PREFIXES = ["sssom", "owl", "rdf", "rdfs", "skos"]
+SSSOM_BUILT_IN_PREFIXES = ["sssom", "owl", "rdf", "rdfs", "skos", "semapv"]
 DEFAULT_MAPPING_SET_ID = f"{SSSOM_URI_PREFIX}mappings/{uuid.uuid4()}"
 DEFAULT_LICENSE = f"{SSSOM_URI_PREFIX}license/unspecified"
 
