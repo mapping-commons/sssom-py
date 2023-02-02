e8ed23d68a6428465e22570097cd3f3712ad4207
Harshad Hegde <hegdehb@gmail.com>
2023-02-02 10:02:46 -0600

    corrected setup

--- sssom/parsers.py
+++ sssom/parsers.py
@@ -310,7 +310,7 @@ def _get_mdict_ms_and_bad_attrs(
 
     mdict = {}
     sssom_schema_object = (
-        SSSOMSchemaView.instance if SSSOMSchemaView.instance else SSSOMSchemaView()
+        SSSOMSchemaView.instance if hasattr(SSSOMSchemaView,"instance") else SSSOMSchemaView()
     )
     for k, v in row.items():
         if v and v == v:
