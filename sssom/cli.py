f6bcf2b5708c7531e42d9803973047b112301b01
Harshad Hegde <hegdehb@gmail.com>
2023-04-18 10:40:17 -0500

    typo

--- sssom/cli.py
+++ sssom/cli.py
@@ -595,7 +595,7 @@ def reconcile_prefixes(input: str, reconcile_prefix_file: Path, output: TextIO):
     "-r",
     "--by-rows",
     default=True,
-    help="Sort rows by DataFrame column #1 (ascending).",
+    help="Sort rows by DataFrame all columns (ascending).",
 )
 def sort(input: str, output: TextIO, by_columns: bool, by_rows: bool):
     """
