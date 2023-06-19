6aa494d94cc55b29ef9ee7583b6c0e9ab314a559
Harshad Hegde <hegdehb@gmail.com>
2023-06-19 13:40:46 -0500

    redundant

--- docs/conf.py
+++ docs/conf.py
@@ -54,9 +54,6 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-templates_path = ['_templates']
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "setup.cfg"]
 
 # -- Options for HTML output -------------------------------------------------
