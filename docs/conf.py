012ea3073131d7ba9f1bce79c7ccdcb67c09bc03
Harshad Hegde <hegdehb@gmail.com>
2023-06-19 13:41:02 -0500

    redundant

--- docs/conf.py
+++ docs/conf.py
@@ -68,8 +68,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 html_theme_options = {
     "use_edit_page_button": True,
