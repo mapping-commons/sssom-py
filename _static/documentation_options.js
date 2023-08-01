204e85bb3dd420eac69b702a144579964a3ba93a
hrshdhgd <hrshdhgd@users.noreply.github.com>
2023-08-01 15:19:45 +0000

    Deploying to gh-pages from @ mapping-commons/sssom-py@8a98cf5b59ffbf664af8a846debde8b9b59b3681 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.33rc1.post1.dev0+968123b',
+    VERSION: '0.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
