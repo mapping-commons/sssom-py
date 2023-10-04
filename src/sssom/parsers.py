a28874d306c765df5ef615e7fc3cf79ae5e318f3
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-10-04 13:34:16 +0300

    Add default value for mapping justification in ensure_valid_mapping

--- src/sssom/parsers.py
+++ src/sssom/parsers.py
@@ -924,6 +924,8 @@ def _ensure_valid_mapping_from_dict(mdict):
     :param mdict: A dictionary containing the mapping metadata.
     :return: A valid Mapping object, or None.
     """
+    mdict.setdefault(MAPPING_JUSTIFICATION, MAPPING_JUSTIFICATION_UNSPECIFIED)
+
     try:
         m = Mapping(**mdict)
         return m
