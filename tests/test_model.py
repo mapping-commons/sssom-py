22925ea1d883d07e6441854666f8ba9734e6902a
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2023-10-04 13:00:16 +0300

    Refactor sssom model tests
    Add a check that information on missing elements is provided
    split checks in two

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,21 +8,26 @@ from sssom_schema import Mapping
 class TestModel(unittest.TestCase):
     """A test case for making sure the model works as intended."""
 
-    def test_mapping_throws_value_error(self):
+    def test_invalid_mapping_throws_value_error(self):
         """Test if instantiating Mapping() fails when required elements are missing."""
         mdict_missing = dict(
             subject_id="ID:123"
         )  # This is missing object_id, predicate_id, mapping_justification
+
+        with self.assertRaises(ValueError):
+            _ = Mapping(**mdict_missing)
+
+        with self.assertRaisesRegex(ValueError, "must be supplied"):
+            _ = Mapping(**mdict_missing)
+
+    def test_valid_mapping_does_not_throw_value_error(self):
+        """Test if instantiating Mapping() works when all required elements are present."""
         mdict_complete = dict(
             subject_id="AID:123",
             object_id="BID:123",
             predicate_id="skos:exactMatch",
             mapping_justification="semapv:LexicalMatching",
         )
-
-        with self.assertRaises(ValueError):
-            m = Mapping(**mdict_missing)
-
         m = Mapping(**mdict_complete)
         self.assertEqual(m.subject_id, "AID:123")
         self.assertEqual(m.object_id, "BID:123")
