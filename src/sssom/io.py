a926aa4acd671d00f697324989d09431e0ffac9c
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-11-06 11:28:33 +0100

    Update io.py

--- src/sssom/io.py
+++ src/sssom/io.py
@@ -12,6 +12,7 @@ import curies
 import pandas as pd
 import yaml
 from curies import Converter
+from deprecation import deprecated
 from pansql import sqldf
 
 from sssom.validators import validate
@@ -159,6 +160,17 @@ def _merge_converter(converter: Converter, prefix_map_mode: str = None) -> Conve
     if prefix_map_mode == PREFIX_MAP_MODE_MERGED:
         return curies.chain([converter, get_converter()])
     raise ValueError(f"Invalid prefix map mode: {prefix_map_mode}")
+
+
+@deprecated(deprecated_in="0.4.0", details="Use sssom.io.extract_iris() directly")
+def get_list_of_predicate_iri(predicate_filter: Iterable[str], converter: Converter) -> list:
+    """Return a list of IRIs for predicate CURIEs passed.
+
+    :param predicate_filter: CURIE OR list of CURIEs OR file path containing the same.
+    :param converter: Prefix map of mapping set (possibly) containing custom prefix:IRI combination.
+    :return: A list of IRIs.
+    """
+    return extract_iris(predicate_filter, converter)
 
 
 def extract_iris(
